00293c33f3f06ae71ad6f5373fe5c4acf71a1d5e
Alexey <sotchenkoff@gmail.com>
2023-11-25 13:30:19 +0300

    added config parser

---
+++ internal/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml: "env" env-default:"local"`
+	StoragePath string `yaml: "storage" env-required:"true"`
+	HTTPServer  `yaml: "http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml: "address" env-default:'localhost:8080"`
+	Timeout     time.Duration `yaml: "timeout" env-default:'4s"`
+	IdleTimeout time.Duration `yaml: "idle_timeout" env-default:'60s"`
+}
+
+func MustLoad() Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH must be set")
+	}
+
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file %s does not exist", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config file %s", err)
+	}
+
+	return cfg
+}
