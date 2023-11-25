818a723b7fe5a076b9ea6c2c978b7170cc499c10
Alexey <sotchenkoff@gmail.com>
2023-11-25 14:18:33 +0300

    added logger init

--- cmd/ush/main.go
+++ cmd/ush/main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
+	"os"
+	"ush/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
+	cfg := config.MustLoad()
 
+	log := setupLogger(cfg.Env)
+	log.Info("starting ush", slog.String("env", cfg.Env))
+
+	fmt.Println(cfg)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
