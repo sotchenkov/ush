3a3436aca0c3f4b2355f82ca3201533b19580584
Alexey <sotchenkoff@gmail.com>
2023-11-25 20:57:06 +0300

    added handler

--- cmd/ush/main.go
+++ cmd/ush/main.go
@@ -5,8 +5,13 @@ import (
 	"log/slog"
 	"os"
 	"ush/internal/config"
+	"ush/internal/http-server/handlers/url/save"
+	mwLogger "ush/internal/http-server/middleware/logger"
 	"ush/internal/lib/logger/sl"
 	"ush/internal/storage/sqlite"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
@@ -28,20 +33,24 @@ func main() {
 		log.Error("Failed to init db", sl.Err(err))
 		os.Exit(1)
 	}
-	
-	err = storage.SaveURL("https://www.google.com", "google")
-	if err != nil {
-		log.Error("Failed to init db", sl.Err(err))
-		os.Exit(1)
-	}
 
-	err = storage.SaveURL("https://www.google.com", "google")
-	if err != nil {
-		log.Error("Failed to init db", sl.Err(err))
-		os.Exit(1)
-	}
+	// err = storage.SaveURL("https://www.google.com", "google")
+	// if err != nil {
+	// 	log.Error("Failed to init db", sl.Err(err))
+	// 	os.Exit(1)
+	// }
 
 	_ = storage
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	// router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	router.Post("/url", save.New(log, storage))
 }
 
 func setupLogger(env string) *slog.Logger {
---
+++ internal/http-server/handlers/url/save/save.go
@@ -0,0 +1,100 @@
+package save
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+	resp "ush/internal/lib/api/response"
+	"ush/internal/lib/logger/sl"
+	"ush/internal/lib/random"
+	"ush/internal/storage"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+)
+
+type Request struct {
+	URL   string `json:"url" validate:"required, url"`
+	Alias string `json:"alias,omitempty"`
+}
+
+type Response struct {
+	resp.Response
+	Alias string `json:"alias,omitempty"`
+}
+
+type URLSaver interface {
+	SaveURL(urlToSave string, alias string) error
+}
+
+const aliasLength = 7
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const fn = "handlers.url.save.New"
+
+		log = log.With(
+			slog.String("fn", fn),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body: ", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request"))
+
+			return
+		}
+
+		log.Info("request body decoded successfully", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+
+			log.Error("Invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("invalid request"))
+			render.JSON(w, r, resp.ValidationError(validateErr))
+
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			//TODO: check if url already exist
+			alias = random.NewRandomString(aliasLength)
+		}
+
+		err = urlSaver.SaveURL(req.URL, alias)
+		if errors.Is(err, storage.ErrURLAlreadyExist) {
+			log.Info("url already exists", slog.String("url", req.URL))
+
+			render.JSON(w, r, resp.Error("url already exists"))
+
+			return
+		}
+
+		if err != nil {
+			log.Error("failed to add url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to add url"))
+
+			return
+		}
+
+		log.Info("url added")
+
+		responseOK(w, r, alias)
+	}
+}
+
+func responseOK(w http.ResponseWriter, r *http.Request, alias string) {
+	render.JSON(w, r, Response{
+		Response: resp.OK(),
+		Alias:    alias,
+	})
+}
